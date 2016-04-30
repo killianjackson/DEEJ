4cc12b7a0c42a805a66456a2b1a8dca603b3b224
Killian Jackson <killianjackson@yahoo.com>
2016-04-29 20:49:47 -0700

    Initial Commit

---
+++ DEEJ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DEEJ
+//
+//  Created by Killian Jackson on 4/29/16.
+//  Copyright © 2016 Killian Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
